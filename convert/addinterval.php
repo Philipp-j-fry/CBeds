3c152a0990f9128972d8493764b7fbd82c2238b9
philipp <fil.jlh@bk.ru>
2015-05-15 17:02:25 +0300

    add causage

--- convert/addinterval.php
+++ convert/addinterval.php
@@ -54,7 +54,7 @@ class add_interval extends WebDriverTestCase {
     // clickElement
     $this->byName("arates")->click();
     // clickElement
-    $this->byCssSelector("#main_menu #sroomRates a")->click();
+    $this->byCssSelector("#main_menu #sroomRates")->click();
     // waitForElementAttribute
     $this->waitUntil(function() use ($test) {
       try {
