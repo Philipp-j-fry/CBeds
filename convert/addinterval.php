6fe29c44d63c8e89f85f4837b8b9b8134166a97f
philipp <fil.jlh@bk.ru>
2015-05-15 17:35:16 +0300

    add causage

--- convert/addinterval.php
+++ convert/addinterval.php
@@ -61,7 +61,7 @@ class add_interval extends WebDriverTestCase {
           return true;
       },50000);
     // clickElement
-    $this->byXPath("//*[@id='srates']")->click();
+    $this->byXPath("//*[@id='srates']/a")->click();
     // clickElement
     $this->byXPath("//*[@id='sroomRates']/a")->click();
     // waitForElementAttribute
